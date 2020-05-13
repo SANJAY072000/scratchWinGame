3c5611b9752f0a459dcb1d5873fec0b1c814048d
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-05-13 15:31:05 +0530

    Done with default app setup

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends Component {
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>Scratch Win Game</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
